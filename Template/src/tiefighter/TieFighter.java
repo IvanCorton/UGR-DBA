8cb55dfb9c1eb0ce5b6f6129578726a1a017af02
Iván Cortón da Silva <ivancorton99@gmail.com>
2021-11-15 21:45:06 +0100

    Updating Template

---
+++ Template/src/tiefighter/TieFighter.java
@@ -0,0 +1,16 @@
+package tiefighter;
+
+import appboot.LARVABoot;
+
+
+public class TieFighter {
+    public static void main(String[] args) {
+        LARVABoot connection = new LARVABoot();
+        
+        connection.Boot("isg2.ugr.es", 1099);
+        connection.launchAgent("MiniYoda", MyFirstTieFighter.class);
+        connection.WaitToShutDown();
+       
+    }
+    
+}
